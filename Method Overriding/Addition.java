b2f1ed6232bee4687297206da9190ebf9874496e
wakeupbhavesh <wakeupbhavesh@gmail.com>
2026-04-23 00:09:27 +0530

    Implement Addition class with method overloading

---
+++ Method Overriding/Addition.java
@@ -0,0 +1,24 @@
+
+public class Addition {
+    public void add()
+    {
+    	int a = 10;
+    	int b = 20;
+    	int c = a+b;
+    	System.out.println(" Addition is "+c);
+    }
+    public void add(int x)
+    {
+    	int a = x;
+    	int b = x;
+        int c = a+b;
+    	System.out.println(" Addition is "+c);
+    }
+    public void add(int x,int y)
+    {
+    	int a = x;
+    	int b = y;
+        int c = a+b;
+    	System.out.println(" Addition is "+c);
+    }
+}
