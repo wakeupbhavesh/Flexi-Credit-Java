16835ea8e07fe4ac94360bb84dc5288155fe0fbf
wakeupbhavesh <wakeupbhavesh@gmail.com>
2026-04-23 00:13:03 +0530

    Create ArithmeticOperationsMain.java

---
+++ Methods in java/ArithmeticOperationsMain.java
@@ -0,0 +1,11 @@
+
+public class ArithmeticOperationsMain {
+
+	public static void main(String[] args) {
+		ArithmeticOperation obj1 = new ArithmeticOperation();
+		obj1.addition(20, 20);
+		obj1.subtraction(50,10);
+
+	}
+
+}
