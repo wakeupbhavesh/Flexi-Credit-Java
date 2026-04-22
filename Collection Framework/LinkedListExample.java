3022bd2c10abee537e42c67388a7618ca56ef1b1
wakeupbhavesh <wakeupbhavesh@gmail.com>
2026-04-23 00:02:18 +0530

    Add LinkedListExample class with basic operations

---
+++ Collection Framework/LinkedListExample.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.add("Java");
+		linkedList.add("Python");
+		linkedList.add("C++");
+		linkedList.add("DBMS");  
+		System.out.println(linkedList);
+		System.out.println(linkedList.get(0));
+		
+
+	}
+
+}
