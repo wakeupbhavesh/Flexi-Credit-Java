a12d8a38910995a1049e55e8231463430f24f5a0
wakeupbhavesh <wakeupbhavesh@gmail.com>
2026-04-23 00:12:39 +0530

    Create ArithmeticOperation.java

---
+++ Methods in java/ArithmeticOperation.java
@@ -0,0 +1,20 @@
+
+public class ArithmeticOperation {
+    private int a;
+    private int b;
+    private int c;
+    public void addition(int p1,int p2)
+    {
+    	a = p1;
+    	b = p2;
+    	c = a+b;
+    	System.out.println(" addition is : "+c);
+    }
+    public void subtraction(int p1,int p2)
+    {
+    	a = p1;
+    	b = p2;
+    	c = a-b;
+    	System.out.println(" subtraction is : "+c);
+    }
+}
