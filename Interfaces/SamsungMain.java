cd0df67a40246ee34fb1e156e3284262b6d467e1
wakeupbhavesh <wakeupbhavesh@gmail.com>
2026-04-23 00:07:41 +0530

    Add SamsungMain class with main method

---
+++ Interfaces/SamsungMain.java
@@ -0,0 +1,14 @@
+
+public class SamsungMain {
+
+	public static void main(String[] args) {
+		Samsung s1 = new Samsung();
+		s1.scanStation();
+		s1.playStation();
+		s1.captureImage();
+		s1.showImage();
+		s1.showMaps();
+
+	}
+
+}
